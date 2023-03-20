e5ec41ef3b59b195054f1f859eaeee82b4a24fd0
santosh <santoshsahtpss@gmail.com>
2023-03-20 15:56:49 +0530

    some changes in designing

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Hero.css'
+import waving from '../../assets/waving.png'
+import heroImg from '../../assets/heroimg.png'
 const Hero = () => {
     return (
         <div className="hero-section">
@@ -7,14 +9,16 @@ const Hero = () => {
                 <div className="hero-main">
                     <div className="hero-section-content">
                         <div className="left-h">
-                            <h1>Front-End React Developer</h1>
+                            <h1>Front-End React Developer <img src={waving} alt="" /></h1>
                             <p>Hi, I'm Santosh. A passionate Front-end React Developer based in Belgrade, Serbia. 📍</p>
                             <div className="portfolio-link">
                                 <a href="/"><i class="fa-brands fa-linkedin"></i></a>
                                 <a href="/"><i class="fa-brands fa-github"></i></a>
                             </div>
                         </div>
-                        <div className="right-h"></div>
+                        <div className="right-h">
+                            {/* <img src={heroImg} alt="" /> */}
+                        </div>
                     </div>
                     <div className="skills">
                         <p>Tech Stack</p>
